4277c38ad7f786619667ad648e1da315e1b3bb27
Alexey Chernyadyev <jashka34@gmail.com>
2018-01-28 12:37:08 +0300

    Добавлен эксперемент через then()

--- async-wait/index.ts
+++ async-wait/index.ts
@@ -1,27 +1,42 @@
 function subp(subName : string) : Promise<string> {
     return new Promise<string>((resolve, error) => {
-            setTimeout(()=> resolve(subName + " subscribe OK"),1000*5);
+            setTimeout(()=> resolve(subName + " subscribe OK"),1000*2);
         });
 }
 
-async function subs(subArr : [string]) {
-
+async function subs(subArr : string[]) {
+    console.time('subs');
     console.log("go subs");
-
     for(let subName of subArr) {
         let res = await subp(subName);
         console.log(res);
     }
-
+    console.timeEnd('subs');
     console.log("end subs");
-
 }
 
-subs([
-    "user.route",
-    "user.asset",
-    "user.checkresults",
-    "user.operations",
-    "user.blablabla"
-]);
+async function subs2(subName : string) {
+    let res = await subp(subName);
+    console.log(res);
+}
 
+let subsArr = [ "user.route",
+                "user.asset",
+                "user.checkresults",
+                "user.operations",
+                "user.blablabla"
+              ];
+
+console.log("-->");
+subs(subsArr);
+console.log("<--");
+
+
+// console.log("---->");
+// subs2("user.route")
+//   .then((data) => subs2("user.asset"))
+//   .then((data) => subs2("user.checkresults"))
+//   .then((data) => subs2("user.operations"))
+//   .then((data) => subs2("user.blablabla"))
+//   .catch(console.error);
+// console.log("<----");
